6c6bbf3b5fb4cc30636f626a76ec1244627fd7d5
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-06-27 21:59:10 +0530

    changes

--- src/pages/IssueDetail.js
+++ src/pages/IssueDetail.js
@@ -14,6 +14,7 @@ class IssueDetail extends React.Component {
     } 
 
     const issue = issues.find(i=>i.id === parseInt(issue_id));
+    console.log(issue);
     if(!issue){
         return <p>Not Found </p>;
     } 
