0689e98056a24f43f7d0ad9a698a8de2697d36a4
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-08-22 18:42:43 +0530

    Bug fixes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, IndexRoute, IndexRedirect } from 'react-router';
 
 import App from './pages/App';
-import List from './pages/List';
-import Detail from './pages/Detail';
-import User from './pages/User';
+// import List from './pages/Issue';
+// import Detail from './pages/IssueDetail';
+// import User from './pages/User';
 import Issue from './pages/Issue';
 import IssueDetail from './pages/IssueDetail';
 
